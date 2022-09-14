b88c5dfd5db1891be26dc6c7f58937430de23018
bokigolic <bokigolic32@gmail.com>
2022-09-13 22:58:40 -0400

    add new component, works on scss

--- parking-garage/src/components/ParkingGarage.js
+++ parking-garage/src/components/ParkingGarage.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import ParkingGarageFlor from "./ParkingGarageFlor";
 
 /*Objectives
 -
@@ -9,11 +10,11 @@ const ParkingGarage = () => {
 
   const initialState = {
     flor1: {
-      florA: [null, null, null, null, null, null, null, null, null, null],
+      florA: [null, null, "test123", null, null, null, null, null, null, null],
       florB: [null, null, null, null, null, null, null, null, null, null]
     },
     flor2: {
-      florA: [null, null, null, null, null, null, null, null, null, null],
+      florA: ["BMW123", null, null, null, null, null, null, null, null, null],
       florB: [null, null, null, null, null, null, null, null, null, null]
     },
     flor3: {
@@ -35,6 +36,37 @@ const ParkingGarage = () => {
       <h3>Parking Garage</h3>
       <p>Display of parking spaces in the shopping center</p>
       <h3>Monitor Garage</h3>
+      <div className="monitor-garage">
+        <div className="flor">
+          <header>Flor 1</header>
+          <footer>
+            <ParkingGarageFlor />
+            <ParkingGarageFlor />
+          </footer>
+        </div>
+        <div className="flor">
+          <header>Flor 2</header>
+          <footer>
+            <ParkingGarageFlor />
+            <ParkingGarageFlor />
+          </footer>
+        </div>
+        <div className="flor">
+          <header>Flor 3</header>
+          <footer>
+            <ParkingGarageFlor />
+            <ParkingGarageFlor />
+          </footer>
+        </div>
+        <div className="flor">
+          <header>Flor 4</header>
+          <footer>
+            <ParkingGarageFlor />
+            <ParkingGarageFlor />
+          </footer>
+        </div>
+
+      </div>
 
       <h3>Widget for prak new car</h3>
     </div >
---
+++ parking-garage/src/components/ParkingGarageFlor.js
@@ -0,0 +1,18 @@
+const ParkingGarageFlor = () => {
+  return (
+    <div className="middle-flor">
+      <header>
+         A
+      </header>
+      <div className="parking-spot-list">
+        <div className="spot">
+          example  12333
+
+        </div>
+      </div>
+
+    </div>
+
+  )
+};
+export default ParkingGarageFlor;
